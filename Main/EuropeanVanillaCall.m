d74f903dbcbd4673ceaf70edcd880bd87acb20dd
chenpenghao <xmchenph@gmail.com>
2017-10-06 11:07:55 +0800

    [ass1] add group member to first line

--- Main/EuropeanVanillaCall.m
+++ Main/EuropeanVanillaCall.m
@@ -1,3 +1,6 @@
+%Group members: Chen Penghao, Wang Zexin
+%Group number: G01
+
 function [c] = EuropeanVanillaCall(S0, q, X, tau, r, sigma)
   d1 = (log(S0./X) + (r - q + sigma ^ 2 / 2) * tau) / (sigma * tau ^ 0.5);
   d2 = d1 - sigma * tau ^ 0.5;
